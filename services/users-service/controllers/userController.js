6b9ce8c8ea8686797601e05f69c3d21702775a5e
yash <yashmhatre@outlook.com>
2020-05-05 17:03:41 +0530

    added-signup-api

--- services/users-service/controllers/userController.js
+++ services/users-service/controllers/userController.js
@@ -4,7 +4,7 @@ module.exports = {
     userSignUp: function(req, res) {
         userService.signUp(req).then(
             (data) => {
-                res.status(200).send(data);
+                res.status(201).send(data);
             },
             (err) => {
                 res.status(400).send({ errorCode: 1, message: 'Invalid Input' });
--- services/users-service/service/userService.js
+++ services/users-service/service/userService.js
@@ -1,5 +1,51 @@
+var auth = require("./../../custom-modules/auth/index");
+const db = require("./../../custom-modules/database/index");
+
 module.exports = {
     signUp: function(req) {
-        return new Promise((resolve, reject) => {});
+        return new Promise((resolve, reject) => {
+            const user = req.body;
+            auth
+                .hashPassword(user.password)
+                .then((hashedPassword) => {
+                    user.password_digest = hashedPassword;
+                })
+                .then(() => auth.createToken(user.username, user.userrole))
+                .then((token) => (user.token = token))
+                .then(() => createUser(user))
+                .then((user) => {
+                    delete user.password_digest;
+                    resolve(user);
+                })
+                .catch((err) => {
+                    console.error(err);
+                    reject(err);
+                });
+        });
     },
 };
+
+function createUser(user) {
+    return new Promise(async(resolve, reject) => {
+        user.username = user.firstname + user.lastname;
+        let query =
+            "INSERT INTO USERS (FIRSTNAME,LASTNAME,USERNAME,EMAILID,MOBILENUMBER,USERROLE,TOKEN,ISLOGGEDIN,PASSWORD) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *";
+        let values = [
+            user.firstname,
+            user.lastname,
+            user.username,
+            user.email,
+            user.mobile_number,
+            user.userrole,
+            user.token,
+            true,
+            user.password_digest,
+        ];
+        try {
+            await db.parameterizedQuery(query, values);
+            resolve(user);
+        } catch (error) {
+            reject(error);
+        }
+    });
+}
