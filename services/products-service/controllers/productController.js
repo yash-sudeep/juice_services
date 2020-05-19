4f2a8b71a17c6cabe7717d0f01319bfe285b1b5f
yash <yashmhatre@outlook.com>
2020-05-19 19:41:49 +0530

    status code issue fixed

--- services/products-service/controllers/productController.js
+++ services/products-service/controllers/productController.js
@@ -5,7 +5,7 @@ module.exports = {
     getAllProducts: function(req, res) {
         productService.getAllProducts(req).then(
             (data) => {
-                res.status(201).send(data);
+                res.status(200).send(data);
             },
             (err) => {
                 res.status(400).send({ errorCode: 1, message: "Invalid Input" });
@@ -15,7 +15,7 @@ module.exports = {
     getProgramWiseProducts: function(req, res) {
         productService.getProgramWiseProducts(req).then(
             (data) => {
-                res.status(201).send(data);
+                res.status(200).send(data);
             },
             (err) => {
                 res.status(400).send({ errorCode: 1, message: "Invalid Input" });
@@ -25,7 +25,7 @@ module.exports = {
     getProductDescription: function(req, res) {
         productService.getProductDescription(req).then(
             (data) => {
-                res.status(201).send(data);
+                res.status(200).send(data);
             },
             (err) => {
                 res.status(400).send({ errorCode: 1, message: "Invalid Input" });
