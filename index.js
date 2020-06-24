8cb2b704eb8dedb15d9eec5cee279cb31f53846a
yash <yashmhatre@outlook.com>
2020-06-24 19:54:22 +0530

    serving frontend

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var app = express();
 var router = express.Router();
 var tokenValidation = require('./services/custom-modules/auth').validateToken;
 
+// enabling CORS
 var allowCrossDomain = function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
@@ -15,12 +16,14 @@ var allowCrossDomain = function(req, res, next) {
 
 app.use(allowCrossDomain);
 
+//  Connect all our routes to our application
 let db = require("./services/custom-modules/database/index.js");
 require("./services/users-service/routes.js")(app, router);
 require("./services/subscription-services/routes.js")(app, router);
 require("./services/products-service/routes.js")(app, router);
 require("./services/programs-service/routes.js")(app, router);
 
+// middleware functions
 app.use(tokenValidation);
 app.use(require("body-parser").json({ limit: '50mb', extended: true }));
 
@@ -28,6 +31,11 @@ app.use(function(err, req, res, next) {
     if (err instanceof SyntaxError && err.status === 400 && "body" in err) {
         res.status(400).send({ code: 400, message: "Invalid Input" });
     } else next();
+});
+
+// serving frontend
+router.get('*', function(req, res) {
+    res.sendFile('index.html', { root: __dirname + '/public/' });
 });
 
 app.use("/api", router);
