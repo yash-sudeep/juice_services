50e42407f2e77dab12353a1bf7acb6ca5280dc77
yash-sudeep <64363614+yash-sudeep@users.noreply.github.com>
2020-11-02 17:58:01 +0530

    removed console log statement

--- services/orders-service/routes.js
+++ services/orders-service/routes.js
@@ -7,9 +7,6 @@ module.exports = function(app, router) {
 
     app.use(bodyParser.json({ limit: '50mb' }));
     app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 500000 }));
-
-   
-console.log("in routes")
     
     // Add New order
     router.post('/order/add', cartController.validate("addOrder"), cartController.addOrder);
